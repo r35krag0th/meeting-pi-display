950cdab75a0f11d9a1161a4ec35194069102501b
Bob Saska <git@r35.net>
2020-04-16 23:19:52 -0500

    chore: adding the basic python deps and helper scripts
    Added scripts/raspbian_install.zsh to help get the system libraries installed for these compiled python dependency.
    Starting with PyGame to build this out, inspired by the LCARS project.

---
+++ scripts/raspbian_install.zsh
@@ -0,0 +1,46 @@
+#!/usr/bin/env zsh
+sudo apt-get install -y \
+    build-essential \
+    curl \
+    fluid-soundfont-gm \
+    fontconfig \
+    fonts-freefont-ttf \
+    git \
+    git-lfs \
+    libavcodec-dev \
+    libavformat-dev \
+    libbz2-dev \
+    libffi-dev \
+    libfreetype6-dev \
+    liblzma-dev \
+    libncurses5-dev \
+    libncursesw5-dev \
+    libportmidi-dev \
+    libreadline-dev \
+    libsdl-image1.2-dev \
+    libsdl-mixer1.2-dev \
+    libsdl-ttf2.0-dev \
+    libsdl1.2-dev \
+    libsmpeg-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libswscale-dev \
+    libtiff5-dev \
+    libx11-6 \
+    libx11-dev \
+    llvm \
+    make \
+    python-openssl \
+    python3-dev \
+    python3-numpy \
+    python3-opengl \
+    python3-setuptools \
+    timgm6mb-soundfont \
+    tk-dev \
+    wget \
+    xfonts-100dpi \
+    xfonts-75dpi \
+    xfonts-base \
+    xfonts-cyrillic \
+    xz-utils \
+    zlib1g-dev
